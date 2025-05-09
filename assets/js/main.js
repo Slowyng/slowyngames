753e4325a7d5223be2e14972a98c9da86ffc62f5
Mudassir Nawaz Javaid <mudassirnawazjavaid@gmail.com>
2025-05-09 14:30:47 +0500

    changes made, projects image edit remining

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,22 @@ document.addEventListener('DOMContentLoaded', () => {
     const menuToggler = document.querySelector('.toggle-menu');
     const menu = document.querySelector('.menu');
 
+    document.addEventListener('scroll', () => {
+        const opener = menuToggler.querySelector('.open-menu');
+        const closer = menuToggler.querySelector('.close-menu');
+        if (!menu.classList.contains('translate-x-full')) {
+            menu.classList.add('translate-x-full');
+        };
+
+        if (opener.classList.contains('hidden')) {
+            opener.classList.remove('hidden');
+        };
+         
+        if (!closer.classList.contains('hidden')) {
+            closer.classList.add('hidden');
+        };
+    });
+
     menuToggler.addEventListener('click', () => {
         menuToggler.querySelector('.open-menu')
             .classList.toggle('hidden');
